6d7c8d0f9d2af98a9a6b1afe68dc911a5a3c7896
sahilempire <66163158+sahilempire@users.noreply.github.com>
2022-05-24 17:07:24 +0530

    something more

--- app/src/main/java/com/example/mentalhealthdetector/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthdetector/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.mentalhealthdetector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC, ansD;
+    Button submitBtn;
+
+    int score=0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+        ansD = findViewById(R.id.ans_D);
+        submitBtn = findViewById(R.id.submit_btn);
+
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+
+        totalQuestionsTextView.setText("Total Questions:  "+totalQuestion);
+
+        loadNewQuestion();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    void loadNewQuestion(){
+
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+
     }
 }
---
+++ app/src/main/java/com/example/mentalhealthdetector/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.mentalhealthdetector;
+
+public class QuestionAnswer {
+
+    Public static String question[] = {
+            "which company owns the android?",
+            "which one is not the Programming Language?",
+            "Where are you From"
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "Notepad", "Python"},
+            {"Agra", "Kota", "Guna", "Jaipu"}
+    };
+
+    public static String correctAnswer[] = {
+            "Google",
+            "Notepad",
+            "Agra"
+    };
+}
